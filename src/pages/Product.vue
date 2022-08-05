df90316eec8b031eb8eac2e5f893afeae319f6f3
madshox <63535913+madshox@users.noreply.github.com>
2022-08-05 12:05:40 +0500

    feat: add slider for product img's

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -2,9 +2,27 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <img :src="product.img" :alt="product.title" />
-        <h1>{{ product.title }}</h1>
-        <p>{{ product.descr }}</p>
+        <div class="product__wrapper">
+          <!-- slider -->
+          <div class="product-slider">
+            <carousel
+              :perPage="1"
+              :paginationEnable="true"
+              paginationColor="#b3b3b3"
+              paginationActiveColor="#494ce8"
+            >
+              <slide v-for="(slide, index) in product.gallery" :key="index">
+                <img :src="slide.img" :alt="slide.name" />
+              </slide>
+            </carousel>
+          </div>
+
+          <!-- content -->
+          <div class="product-content">
+            <h1 class="title">{{ product.title }}</h1>
+            <p>{{ product.descr }}</p>
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -23,3 +41,21 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.product__wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.product-slider,
+.product-content {
+  max-width: 48%;
+  text-align: center;
+}
+.VueCarousel-inner {
+  visibility: visible !important;
+  flex-basis: 100% !important;
+  width: 100% !important;
+}
+</style>
--- src/store/shop.js
+++ src/store/shop.js
@@ -5,32 +5,51 @@ export default {
                 id: 1,
                 title: 'Nike Red',
                 descr: 'Nike Red Man by Nike is a Woody Spicy fragrance for men. Nike Red Man was launched in 2015. Top notes are Lime, Lemon and Marigold; middle notes are Cinnamon, Black Cardamom, Peru Balsam and Walnut; base notes are Palisander Rosewood, Patchouli and Ebony.',
-                img: require('../assets/shop-img/1.png')
+                img: require('../assets/shop-img/1.png'),
+                gallery: [
+                    { name: 'Nike boots First', img: require('../assets/shop-img/1.png') },
+                    { name: 'Nike boots Second', img: require('../assets/shop-img/2.png') },
+                    { name: 'Nike boots Thrid', img: require('../assets/shop-img/3.png') },
+                ]
             },
             {
                 id: 2,
                 title: 'Nike Default',
                 descr: 'Nike Red Man by Nike is a Woody Spicy fragrance for men. Nike Red Man was launched in 2015. Top notes are Lime, Lemon and Marigold; middle notes are Cinnamon, Black Cardamom, Peru Balsam and Walnut; base notes are Palisander Rosewood, Patchouli and Ebony.',
-                img: require('../assets/shop-img/4.png')
+                img: require('../assets/shop-img/4.png'),
+                gallery: [
+                    { name: 'Nike boots First', img: require('../assets/shop-img/4.png') },
+                    { name: 'Nike boots Second', img: require('../assets/shop-img/5.png') },
+                    { name: 'Nike boots Thrid', img: require('../assets/shop-img/6.png') },
+                ]
             },
             {
                 id: 3,
                 title: 'Nike Green',
                 descr: 'Nike Red Man by Nike is a Woody Spicy fragrance for men. Nike Red Man was launched in 2015. Top notes are Lime, Lemon and Marigold; middle notes are Cinnamon, Black Cardamom, Peru Balsam and Walnut; base notes are Palisander Rosewood, Patchouli and Ebony.',
-                img: require('../assets/shop-img/7.png')
+                img: require('../assets/shop-img/7.png'),
+                gallery: [
+                    { name: 'Nike boots First', img: require('../assets/shop-img/7.png') },
+                    { name: 'Nike boots Second', img: require('../assets/shop-img/8.png') },
+                    { name: 'Nike boots Thrid', img: require('../assets/shop-img/9.png') },
+                ]
             },
             {
                 id: 4,
                 title: 'Nike Street',
                 descr: 'Nike Red Man by Nike is a Woody Spicy fragrance for men. Nike Red Man was launched in 2015. Top notes are Lime, Lemon and Marigold; middle notes are Cinnamon, Black Cardamom, Peru Balsam and Walnut; base notes are Palisander Rosewood, Patchouli and Ebony.',
-                img: require('../assets/shop-img/10.png')
+                img: require('../assets/shop-img/10.png'),
+                gallery: [
+                    { name: 'Nike boots Second', img: require('../assets/shop-img/11.png') },
+                    { name: 'Nike boots Thrid', img: require('../assets/shop-img/12.png') },
+                ]
             },
         ]
     },
     mutations: {},
     actions: {},
     getters: {
-        getShopList (state) {
+        getShopList(state) {
             return state.shopList
         },
         getProduct: (state) => (id) => {
